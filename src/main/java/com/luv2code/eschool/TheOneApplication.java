8d1573a4f9ecbb92d6329745ee1ce20ec10deedd
MohammedKhilel <mohammed358041@gmail.com>
2024-06-11 11:21:52 +0300

    Node.js Update

--- src/main/java/com/luv2code/eschool/TheOneApplication.java
+++ src/main/java/com/luv2code/eschool/TheOneApplication.java
@@ -2,6 +2,9 @@ package com.luv2code.eschool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TheOneApplication {
@@ -9,10 +12,19 @@ public class TheOneApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TheOneApplication.class, args);
 		
-		
-		
-		
-		
 	}
 
+	@Bean
+	public WebMvcConfigurer configure() {
+		return new WebMvcConfigurer () {
+			@Override
+			public void addCorsMappings(CorsRegistry reg) {
+				reg.addMapping("/**").allowedOrigins("*");
+			}
+		};
+		
+	}
+	
+	
+	
 }
